3634f34f75545acd10b890945c63ad619d97fba2
laycop <developer@philiplaycock.com>
2014-03-25 18:47:39 -0400

    Update PostController.php
    Added form validation for adding posts and editing posts.
    
    Added functions for editing and deleting posts.

--- app/Controller/PostController.php
+++ app/Controller/PostController.php
@@ -7,6 +7,7 @@ class PostsController extends AppController {
 		$this->set('posts', $this->Post->find('all'));
 	}
 	
+	// view post
 	public function view($id = null) {
 		if (!$id) {
 			throw new NotFoundException(__('Invalid post'));
@@ -19,6 +20,7 @@ class PostsController extends AppController {
 		$this->set('post', $post);
 	}
 	
+	// add post
 	public function add() {
 		if ($this->request->is('post')) {
 			$this->Post->create();
@@ -27,6 +29,46 @@ class PostsController extends AppController {
 				return $this->redirect(array('action' => 'index'));
 			}
 			$this->Session->setFlash(__('Unable to add your post.'));
+		}
+	}
+	
+	// edit post
+	public function edit($id = null) {
+	
+		if (!$id) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+		
+		$post = $this->Post->findById($id);
+		if (!$post) {
+			throw new NotFoundException(__('Invalid post'));
+		}
+		
+		if ($this->request->is(array('post', 'put'))) {
+			$this->Post->id = $id;
+			if ($this->Post->save($this->request->data)) {
+				$this->Session->setFlash(__('Your post has been updated.'));
+				return $this->redirect(array('action' => 'index'));
+			}
+			$this->Session->setFlash(__('Unable to update your post.'));
+		}
+		
+		if (!$this->request->data) {
+			$this->request->data = $post;
+		}
+	}
+	
+	// delete post
+	public function delete($id) {
+		if ($this->request->is('get')) {
+			throw new MethodNotAllowedException();
+		}
+		
+		if ($this->Post->delete($id)) {
+			$this->Session->setFlash(
+				__('The post with id: %s has been deleted.', h($id))
+			);
+			return $this->redirect(array('action' => 'index'));
 		}
 	}
 }
